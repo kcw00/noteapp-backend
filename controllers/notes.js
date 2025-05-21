3f59629432fed7a9c7c8430cc26ed04d04fb80b5
chaewon kim <cheawon0319@gmail.com>
2025-05-20 23:13:16 -0700

    change import

--- controllers/notes.js
+++ controllers/notes.js
@@ -3,7 +3,7 @@ const Note = require('../models/note')
 const User = require('../models/user')
 const { userExtractor } = require('../utils/middleware')
 const { ObjectId } = require('mongodb')
-const { getIo } = require('../socket/socket')
+const { notifyUsers } = require('../sockets/socket')
 
 notesRouter.use(userExtractor)
 
@@ -76,7 +76,7 @@ notesRouter.post('/', async (request, response) => {
     title: body.title,
     content: body.content,
     important: body.important === undefined ? false : body.important,
-    creator: creator,
+    creator: creator?.id,
     collaborators: [], // add empty collaborators array
   })
 
@@ -113,6 +113,12 @@ notesRouter.delete('/:noteId/user/:userId', async (request, response) => {
     // Delete the note from the database
     await Note.findByIdAndDelete(noteId)
 
+    // Send socket.io message to all collaborators
+    // This will help in real-time note updates to all collaborators
+    const userIdsToNotify = [userId, ...note.collaborators.map(c => c.userId.toString())]
+    notifyUsers(userIdsToNotify, 'noteDeleted', { id: noteId })
+    console.log('[socket.io] Note deleted:', noteId)
+
     response.status(204).end() // Successful deletion with no content to return
   } catch (error) {
     console.error('Error deleting note:', error)
@@ -124,7 +130,7 @@ notesRouter.delete('/:noteId/user/:userId', async (request, response) => {
 // Update a note
 notesRouter.put('/:noteId', async (request, response) => {
   const { noteId } = request.params
-  const { title, content, important } = request.body
+  const { important } = request.body
   const userId = request.user.id
 
   const isCreator = await Note.exists({ _id: noteId, creator: userId })
@@ -136,69 +142,80 @@ notesRouter.put('/:noteId', async (request, response) => {
 
   const updatedNote = await Note.findByIdAndUpdate(
     noteId,
-    { title, content, important },
+    { important },
     { new: true, runValidators: true, context: 'query' }
   )
 
   if (!updatedNote) {
     return response.status(404).end()
   }
+
+  // getIo().emit('noteUpdated', updatedNote) // Emit the event to all connected clients
   response.json(updatedNote)
 })
 
 
 // Add collaborator to a note
 notesRouter.put('/:noteId/collaborators', async (request, response) => {
-  const { noteId } = request.params
-  const { collaboratorId, userType } = request.body
-
-  // Check if the collaborator exists
-  const collaborator = await User.findById(collaboratorId)
-  if (!collaborator) {
-    return response.status(404).json({ error: 'Collaborator not found: ' + collaboratorId })
-  }
-
-  // Check if noteId is valid
-  if (!ObjectId.isValid(noteId)) {
-    return response.status(400).json({ error: 'Invalid noteId: ' + noteId })
-  }
-
-
-  const note = await Note.findById(noteId)
-  console.log('Found note:', note)
-  if (!note) {
-    return response.status(404).json({ error: 'Note not found' })
-  }
-
-  // Check if the user is already a collaborator
-  const existingCollaborator = note.collaborators.find(c => c.userId && c.userId.toString() === collaboratorId)
-  if (existingCollaborator) {
-    return response.status(400).json({ error: 'User is already a collaborator' })
-  }
-
-  // Check if the user is the creator of the note
-  const isCreator = note.creator.toString() === request.user._id.toString()
-  if (!isCreator) {
-    return response.status(403).json({ error: 'You do not have permission to add collaborators' })
-  }
-
-  // Check if the userType is valid
-  const validUserTypes = ['editor', 'viewer']
-  if (!validUserTypes.includes(userType)) {
-    return response.status(400).json({ error: 'Invalid userType' })
-  }
-
-  // Add the collaborator
-  note.collaborators.push({
-    userId: collaboratorId,
-    username: collaborator.username,
-    name: collaborator.name,
-    userType
-  })
-  collaborator.shared.push(noteId) // add note to collaborator's shared notes
   try {
+    const { noteId } = request.params
+    const { collaboratorId, userType } = request.body
+
+    // Check if the collaborator exists
+    const collaborator = await User.findById(collaboratorId)
+    if (!collaborator) {
+      return response.status(404).json({ error: 'Collaborator not found: ' + collaboratorId })
+    }
+
+    // Check if noteId is valid
+    if (!ObjectId.isValid(noteId)) {
+      return response.status(400).json({ error: 'Invalid noteId: ' + noteId })
+    }
+
+    const note = await Note.findById(noteId)
+    console.log('Found note:', note)
+    if (!note) {
+      return response.status(404).json({ error: 'Note not found' })
+    }
+
+    // Check if the user is already a collaborator
+    const existingCollaborator = note.collaborators.find(c => c.userId && c.userId.toString() === collaboratorId)
+    if (existingCollaborator) {
+      return response.status(400).json({ error: 'User is already a collaborator' })
+    }
+
+    // Check if the user is the creator of the note
+    const isCreator = note.creator.toString() === request.user._id.toString()
+    if (!isCreator) {
+      return response.status(403).json({ error: 'You do not have permission to add collaborators' })
+    }
+
+    // Check if the userType is valid
+    const validUserTypes = ['editor', 'viewer']
+    if (!validUserTypes.includes(userType)) {
+      return response.status(400).json({ error: 'Invalid userType' })
+    }
+
+    const collaboratorSchema = {
+      userId: collaboratorId,
+      username: collaborator.username,
+      name: collaborator.name,
+      userType
+    }
+
+    // Add the collaborator
+    note.collaborators.push({ ...collaboratorSchema })
+
+    collaborator.shared.push(noteId) // add note to collaborator's shared notes
     const updatedNote = await note.save()
     const updatedCollaborator = await collaborator.save()
+
+    // Send socket.io message to all collaborators
+    // This will help in real-time note updates to all collaborators
+    const userIdsToNotify = [collaboratorId, note.creator.toString(), ...note.collaborators.map(c => c.userId.toString())]
+    console.log('[socket.io] User IDs to notify:', userIdsToNotify)
+    notifyUsers(userIdsToNotify, 'collaboratorAdded', { noteId, collaborator: collaboratorSchema })
+    console.log('[socket.io] Collaborator added:', { noteId, collaborator })
 
     response.status(200).json({ updatedNote, updatedCollaborator })
   } catch (error) {
@@ -208,34 +225,42 @@ notesRouter.put('/:noteId/collaborators', async (request, response) => {
 
 // Remove collaborator from a note
 notesRouter.delete('/:noteId/collaborators', async (request, response) => {
-  const { noteId } = request.params
-  const { collaboratorId } = request.body
-
-  const collaborator = await User.findById(collaboratorId)
-
-  // Check if noteId is valid
-  if (!ObjectId.isValid(noteId)) {
-    return response.status(400).json({ error: 'Invalid noteId' })
-  }
-
-  // Check if the user is the creator of the note
-  const note = await Note.findById(noteId)
-  if (!note) {
-    return response.status(404).json({ error: 'Note not found' })
-  }
-
-  const isCreator = note.creator.toString() === request.user._id.toString()
-  console.log('isCreator:', isCreator)
-  if (!isCreator) {
-    return response.status(403).json({ error: 'You do not have permission to remove collaborators' })
-  }
-
-  // Remove the collaborator
-  note.collaborators = note.collaborators.filter(c => c.userId && c.userId.toString() !== collaboratorId)
-  collaborator.shared = collaborator.shared.filter(n => n.toString() !== noteId) // remove note from collaborator's shared notes
   try {
+    const { noteId } = request.params
+    const { collaboratorId } = request.body
+
+    const collaborator = await User.findById(collaboratorId)
+
+    // Check if noteId is valid
+    if (!ObjectId.isValid(noteId)) {
+      return response.status(400).json({ error: 'Invalid noteId' })
+    }
+
+    // Check if the user is the creator of the note
+    const note = await Note.findById(noteId)
+    if (!note) {
+      return response.status(404).json({ error: 'Note not found' })
+    }
+
+    const isCreator = note.creator.toString() === request.user._id.toString()
+    console.log('isCreator:', isCreator)
+    if (!isCreator) {
+      return response.status(403).json({ error: 'You do not have permission to remove collaborators' })
+    }
+
+    // Remove the collaborator
+    note.collaborators = note.collaborators.filter(c => c.userId && c.userId.toString() !== collaboratorId)
+    collaborator.shared = collaborator.shared.filter(n => n.toString() !== noteId) // remove note from collaborator's shared notes
+
     const updatedNote = await note.save()
     const updatedCollaborator = await collaborator.save()
+
+    // Send socket.io message to all collaborators
+    // This will help in real-time note updates to all collaborators
+    const userIdsToNotify = [collaboratorId, note.creator.toString(), ...note.collaborators.map(c => c.userId.toString())]
+    notifyUsers(userIdsToNotify, 'collaboratorRemoved', { noteId, collaboratorId })
+    console.log('[socket.io] Collaborator removed:', { noteId, collaboratorId })
+
     response.status(200).json({ updatedNote, updatedCollaborator })
   } catch (error) {
     response.status(400).json({ error: 'Failed to remove collaborator' })
